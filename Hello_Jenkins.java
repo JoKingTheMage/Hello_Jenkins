be140045fe42041588339c74cf96c1f490c350b2
KingJo <KingJo606060@gmail.com>
2017-08-19 14:41:43 +0800

    好棒棒，第一次呢

---
+++ Hello_Jenkins.java
@@ -0,0 +1,11 @@
+package easy;
+
+/**
+ * Created by Jo on 2017/8/19.
+ */
+public class Hello_Jenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
